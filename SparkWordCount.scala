3f54ea151dd15570ba7499bb43085fa8c18df3d9
jeremylassiter <jeremylassiter@gmail.com>
2017-03-24 20:57:08 -0400

    Push

---
+++ SparkWordCount.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.sql.SparkSession
+
+object SparkWordCount {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder
+      .master("local")
+      .appName("Spark Graph Frames")
+      .getOrCreate()
+
+    val lines = spark.sparkContext.parallelize(Seq("Spark Intellij Idea Scala test one", "Spark Intellij Idea Scala test two", "Spark Intellij Idea Scala test three"))
+
+    val counts = lines.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+
+    counts.foreach(println)
+  }
+}
